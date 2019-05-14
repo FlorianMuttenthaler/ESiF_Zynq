58175c12cd717c46c89b3f591f069f09d14d9646
FlorianMuttenthaler <flo.muttenthaler@gmail.com>
2019-05-14 15:53:45 +0200

    Task1: implemented grey code software: untested version

--- Task1/ESiF_Task1/ESiF_Task1.sdk/task1/src/main.c
+++ Task1/ESiF_Task1/ESiF_Task1.sdk/task1/src/main.c
@@ -11,8 +11,13 @@
 #define GPIO_CHANNEL 1u
 #define GPIO_DIRMASK 0xFFFFFFFFu //all to 1u as input mask definition
 
+u8 grey_coder_8bit(u8 data);
+
 int main(void)
 {
+	u32 dip_data_new;
+	u32 dip_data;
+	u8 grey_code;
 	/*
 	 * Initialize DIP Switches
 	 */
@@ -22,9 +27,18 @@ int main(void)
 
 	while (TRUE)
 	{
-
+		dip_data_new = XGpio_DiscreteRead(dip, GPIO_CHANNEL);
+		if (dip_data_new != dip_data)
+		{
+			dip_data = dip_data_new;
+			grey_code = grey_coder_8bit((u8) dip_data);
+			xil_printf("%d\n", grey_code);
+		}
 	}
 	return 0;
 }
 
-
+u8 grey_coder_8bit(u8 data)
+{
+	return data ^ (data >> 1);
+}
